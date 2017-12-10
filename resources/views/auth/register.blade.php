0c00f7fac74f98c0dd25f5d53d76a95b7a18834c
Unknown <aa532199@gmail.com>
2017-12-10 22:10:37 +0800

    add the auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,86 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('Id_card_number') ? ' has-error' : '' }}">
+                            <label for="Id_card_number" class="col-md-4 control-label">Id_card_number</label>
+
+                            <div class="col-md-6">
+                                <input id="Id_card_number" type="text" class="form-control" name="Id_card_number" required>
+
+                                @if ($errors->has('Id_card_number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('Id_card_number') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">phone</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control" name="phone" required>
+
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('Birthday') ? ' has-error' : '' }}">
+                            <label for="Birthday" class="col-md-4 control-label">Birthday</label>
+
+                            <div class="col-md-6">
+                                <input id="Birthday" type="date" class="form-control" name="Birthday" required>
+
+                                @if ($errors->has('Birthday'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('Birthday') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('sex') ? ' has-error' : '' }}">
+                            <label for="sex" class="col-md-4 control-label">sex</label>
+
+                            <div class="col-md-6">
+                                <div class="radio">
+                                    <label class="radio-inline">
+                                        <input id="sex" type="radio" name="sex" value="male" checked>male  
+                                    </label>
+                                    <label class="radio-inline">
+                                        <input id="sex" type="radio" name="sex" value="female">female 
+                                    </label> 
+                                    <label class="radio-inline">
+                                        <input id="sex" type="radio" name="sex" value="other">other  
+                                    </label>
+                                </div>
+
+                                @if ($errors->has('sex'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('sex') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+                            <label for="address" class="col-md-4 control-label">address</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}" required>
+
+                                @if ($errors->has('address'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>AATS</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,8 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <img src="wm.jpg" alt="Smiley face" height="30" width="30" style="display:inline;">
+                        AATS
                     </a>
                 </div>
 

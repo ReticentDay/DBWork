4a16994e38fdd69d1ecfe2ff91d3714c4fe7c377
silent <aa532199@gmail.com>
2017-12-27 21:58:23 +0800

    finish other controller

--- app/Http/Controllers/OtherController.php
+++ app/Http/Controllers/OtherController.php
@@ -6,79 +6,19 @@ use Illuminate\Http\Request;
 
 class OtherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function money()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        //●商品則扣增加{GET}：/money
+        if(!Auth::check())
+            return "you can't do it";
+        if(Auth::user()->user_type != 'staff' )
+            return "you can't do it";
+        $orderList = DB::select('select o.state,o.buy_date,sp.quantity,p.product_name,p.price,u.name
+                                form order o,Shop_product sp,shop s,product p,Users u
+                                where o.shop_id = s.shop_id
+                                and sp.shop_id = s.shop_id
+                                and p.product_id = sp.product_id
+                                and s.member_id = u.id');
+        return view('FinancialReportingSystem/index',['orderList' => $orderList]);
     }
 }
